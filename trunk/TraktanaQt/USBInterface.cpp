8dfb28ddfd5e1644fd53baf04335900eaddd906f
victor.ecomp <victor.ecomp@980a965b-2580-2584-76ce-5b5fa40a2959>
2010-11-07 18:52:30 +0000

    SoftwareQt: implementação de testes com a libftdi.

--- trunk/TraktanaQt/USBInterface.cpp
+++ trunk/TraktanaQt/USBInterface.cpp
@@ -19,42 +19,74 @@ USBInterface::USBInterface() {
     vid = 0x0403, pid = 0x6010;
     baudrate = 115200;
     interface = INTERFACE_A;
+
+    ftdih = new Context();
 }
 
 USBInterface::~USBInterface() {
 }
 
 bool USBInterface::connect() {
-//    int ret;
-//    ret = ftdih.open(vid, pid);
-//    if (ret == 0) {
-//        return true;
-//    } else {
-//        return false;
-//    }
-    std::cout << "Searching... \n";
-    List* list = List::find_all(0x0403,0x6010);
-    for (List::iterator it = list->begin(); it != list->end(); it++)
-    {
-        std::cout << "FTDI (" << &*it << "): "
-        << it->vendor() << ", "
-        << it->description() << ", "
-        << it->serial();
-
-        // Open test
-        if(it->open() == 0){
-           std::cout << " (Open OK)";
-        }else{
-           std::cout << " (Open FAILED)";
-           it->close();
+        int ret;
+        std::cout << "Opening... \n";
+        ret = this->ftdih->open(0x0403,0x6010);
+//        ret = ftdih.open("FT2232H MiniModule");
+//        ret = ftdih.open(0x0403,0x6010,"FT2232H MiniModule");
+        std::cout << "ret: " << ret << "\n";
+        if (ret == 0) {
+            std::cout << "true \n";
+            return true;
+        } else {
+            std::cout << "false \n";
+            return false;
         }
-        std::cout << std::endl;
-
-    }
-    std::cout << "Finished\n";
-
-        return false;
-
+//        std::cout << "Searching... \n";
+//        List* list = List::find_all(0x0403,0x6010);
+//        for (List::iterator it = list->begin(); it != list->end(); it++)
+//        {
+//            std::cout << "FTDI (" << &*it << "): "
+//            << it->vendor() << ", "
+//            << it->description() << ", "
+//            << it->serial();
+//
+//            // Open test
+//            if(it->open() == 0){
+//               std::cout << " (Open OK)";
+//            }else{
+//               std::cout << " (Open FAILED)";
+//               it->close();
+//            }
+//            std::cout << std::endl;
+//
+//        }
+//        std::cout << "Finished\n";
+//
+//            return false;
+//    struct ftdi_context ftdih;
+//    // Init
+//    std::cout << "Init...\n";
+//    if (ftdi_init(&ftdih) < 0) {
+//        std::cout << "ftdi_init failed\n";
+//    }
+//
+//    // Select first interface
+//    //ftdi_set_interface(&ftdih, 1);
+//    std::cout << "Opening...\n";
+//    // Open device
+//    int f = ftdi_usb_open(&ftdih, 0x0403, 0x6010);
+//    if (f < 0) {
+//        std::cout << "unable to open ftdi device: \n";
+//    }
+//    // Select first interface
+//    ftdi_set_interface(&ftdih, INTERFACE_A);
+//    // Set baudrate
+//    f = ftdi_set_baudrate(&ftdih, 115200);
+//    if (f < 0) {
+//        std::cout << "unable to set baudrate: \n";
+//    }
+//
+//    std::cout << "Finished\n";
+//    return false;
 }
 
 bool USBInterfacedisconnect() {
