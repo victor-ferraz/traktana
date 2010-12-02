4167b24b0f4070106d9d9c2abd47bdbfb023ed05
victor.ecomp <victor.ecomp@980a965b-2580-2584-76ce-5b5fa40a2959>
2010-12-02 22:38:33 +0000

    Firmware: implementação das rotinas de motores de passo utilizando os Timers2 e 3. Testado!

--- traktana_fw/src/steppers.h
+++ traktana_fw/src/steppers.h
@@ -40,7 +40,47 @@ const unsigned char step_half2[8]= {0b00011111,
 									0b10011111};
 */
 // Protoypes
-void rotate_stepper1(unsigned long steps, unsigned char direction,
-						unsigned int frequency, unsigned char mode);
-void rotate_stepper2(unsigned long steps, unsigned char direction,
-						unsigned int frequency, unsigned char mode);
+//void rotate_stepper1(unsigned long steps, unsigned char direction, unsigned int frequency, unsigned char mode);
+//void rotate_stepper2(unsigned long steps, unsigned char direction, unsigned int frequency, unsigned char mode);
+//
+
+void rotate_stepper1(unsigned long steps, unsigned char direction, unsigned long frequency, unsigned char mode){
+	
+	if(mode){	// Full Stepper Mode
+		if(direction){	// ClockWise Direction in Full Stepper Mode
+			while(steps > 0){
+				if(pos_stepper1 > 3) pos_stepper1 = 0;
+				STEPPERS_PORT&=step_full1[pos_stepper1++];
+				__delay32(frequency);
+				steps--;
+			}
+		}else{			// CounterClockWise Direction in Full Stepper Mode
+			while(steps > 0){
+				if(pos_stepper1 < 0) pos_stepper1 = 3;
+				STEPPERS_PORT&=step_full1[pos_stepper1--];
+				__delay32(frequency);
+				steps--;
+			}
+		}
+	}else{		// Half Stepper Mode
+		if(direction){	// ClockWise Direction in Half Stepper Mode
+			while(steps > 0){
+				if(pos_stepper1 > 7) pos_stepper1 = 0;
+				STEPPERS_PORT&=step_half1[pos_stepper1++];
+				__delay32(frequency);
+				steps--;
+			}
+		}else{			// CounterClockWise Direction in Half Stepper Mode
+			while(steps > 0){
+				if(pos_stepper1 < 0) pos_stepper1 = 7;
+				STEPPERS_PORT&=step_half1[pos_stepper1--];
+				__delay32(frequency);
+				steps--;
+			}
+		}
+	}		
+}
+
+void rotate_stepper2(unsigned long steps, unsigned char direction, unsigned int frequency, unsigned char mode){
+
+}
