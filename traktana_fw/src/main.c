6ce9956af2c1baa82c4f88ee553435e494b3d9e2
victor.ecomp <victor.ecomp@980a965b-2580-2584-76ce-5b5fa40a2959>
2010-12-04 19:38:58 +0000

    Firmware: implementação dos comandos pela porta serial das rotinas de controle dos motores de passo.

--- traktana_fw/src/main.c
+++ traktana_fw/src/main.c
@@ -17,36 +17,52 @@
  * Victor Ferraz         11/10/2010  Original        (Rev 1.0)
  *
  ********************************************************************/
+#include <p30f3014.h>            //standard header file
 
-#include <p30F3014.h>            //Standard header file
 //---------------------------------------------------------------------------
-//Prototypes
+// Variables
+// Global Variables (Stepper Motor control with timers)
+//unsigned char stepper_enable1=0,stepper_enable2=0;
+//unsigned char direction1=1,direction2=1,mode_step1=0,mode_step2=0;
+//unsigned long frequency1,frequency2;
+//---------------------------------------------------------------------------
+// Prototypes
 void InitDevice();
-int init_UART(void);
+int  Init_UART(void);
 void loopback_UART(void);
 void process_cmd(void);
-void step_stepper1();
+void Init_Steppers();
 void Motor_PID_Init (void);
+//--------------------------------------------------------------------------------------------------------------
+//// Timer1 Interrupt routine for stepper 1
+//void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void){
+//
+//	if(stepper_enable1) step_stepper1();
+//	IFS0bits.T1IF = 0;       /* clear interrupt flag     */
+//	return;
+//}
+////--------------------------------------------------------------------------------------------------------------
+//// Timer3 Interrupt routine for stepper 2
+//void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){
+//
+//	step_stepper2();
+//	IFS0bits.T2IF = 0;       /* clear interrupt flag     */
+//	return;
+//}
 //---------------------------------------------------------------------------
-//Variables
-unsigned long freq=200000;
-int slope = 0;
-//---------------------------------------------------------------------------
-//Main routine
-
+// Main routine
 int main(void)
 {
 	InitDevice();
     while(1) 	//Loop forever
     {
-//		loopback_UART();
-//		process_cmd();
+		loopback_UART();
+		process_cmd();
     }
 }
-
+//--------------------------------------------------------------------------------------------------------------
+// Init all necessary functions
 void InitDevice(){
-	TRISB = 0x00; 	// configure PORTB for output (STEPPERS)
-	ADPCFG = 0xFFF;	// configure PORTB for digital output
-	//init_UART();
-	init_Steppers();
+	Init_UART();
+	Init_Steppers();
 }
