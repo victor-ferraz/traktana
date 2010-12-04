c1daa3dc5b8f57f994718f78e95c0e4d30358412
victor.ecomp <victor.ecomp@980a965b-2580-2584-76ce-5b5fa40a2959>
2010-12-04 02:12:41 +0000

    Firmware: início da implementação do controle PID do motor DC. Adição do arquivo motor_pid.c.

--- traktana_fw/src/timers.c
+++ traktana_fw/src/timers.c
@@ -1,33 +1,33 @@
 #include "p30f3014.h"
 #include <timer.h>
 
-// Timer2 Interrupt routine
-void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){
+// Timer1 Interrupt routine
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void){
 
-//	step_stepper1();
-	IFS0bits.T2IF = 0;       /* clear interrupt flag     */
+	step_stepper1();
+	IFS0bits.T1IF = 0;       /* clear interrupt flag     */
 	return;
 }
 // Timer3 Interrupt routine
-void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void){
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){
 
-	step_stepper1();
-	IFS0bits.T3IF = 0;       /* clear interrupt flag     */
+	step_stepper2();
+	IFS0bits.T2IF = 0;       /* clear interrupt flag     */
 	return;
 }
 
 void init_Timers(){
 
+/* Enable Timer1 Interrupt and Priority to "1" */
+    ConfigIntTimer1(T1_INT_PRIOR_1 & T1_INT_ON);
+
+    OpenTimer1(T1_ON & T1_GATE_OFF & T1_PS_1_8 & 
+			 	T1_SOURCE_INT, 0xFFFF);
+
 /* Enable Timer2 Interrupt and Priority to "1" */
     ConfigIntTimer2(T2_INT_PRIOR_1 & T2_INT_ON);
 
-    OpenTimer2(T2_ON & T2_GATE_OFF & T2_PS_1_8 & 
-			 	T2_SOURCE_INT, 0xFFFF);
-
-/* Enable Timer3 Interrupt and Priority to "1" */
-    ConfigIntTimer3(T3_INT_PRIOR_1 & T3_INT_ON);
-
-    OpenTimer3(T3_ON & T3_GATE_OFF & T3_PS_1_8 
-				 & T3_SOURCE_INT, 0xFFFF);
+    OpenTimer2(T2_ON & T2_GATE_OFF & T2_PS_1_8 
+				 & T2_SOURCE_INT, 0xFFFF);
 
 }
